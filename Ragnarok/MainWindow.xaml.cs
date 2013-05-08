396c71d532d3d92f09e03a525c1e7e90c37cfadf
Raven <avalon1610@gmail.com>
2013-05-08 17:03:27 +0800

    Panorama test

--- Ragnarok/MainWindow.xaml.cs
+++ Ragnarok/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using System.Windows.Interop;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace Ragnarok
 {
@@ -24,6 +27,175 @@ namespace Ragnarok
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindowLoaded;
         }
+
+        private void DisableDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void OK_Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            wait_logo.IsActive = false;
+        }
+        private void Cancel_Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            wait_logo.IsActive = true;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+        }
+
+        private static IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Handle Win32 message
+            if (msg == WM_NCRBUTTONDOWN || msg == WM_NCRBUTTONUP || msg == WM_NCRBUTTONDBLCLK ||
+                msg == WM_RBUTTONDOWN || msg == WM_RBUTTONUP || msg == WM_RBUTTONDBLCLK)
+            {
+                handled = true;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private const int HTCAPTION = 2;
+        private const int WM_NCRBUTTONDOWN = 0xA4;
+        private const int WM_NCRBUTTONUP = 0xA4;
+        private const int WM_NCRBUTTONDBLCLK = 0xA6;
+        private const int WM_RBUTTONDOWN = 0x204;
+        private const int WM_RBUTTONUP = 0x205;
+        private const int WM_RBUTTONDBLCLK = 0x206;
+
+        void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = new MainWindowViewModel();
+        }
+    }
+
+    public class MainWindowViewModel
+    {
+        readonly PanoramaGroup _albums;
+        readonly PanoramaGroup _artists;
+        public MainWindowViewModel()
+        {
+            SampleData.Seed();
+            Albums = SampleData.Albums;
+            Artists = SampleData.Artists;
+
+            Busy = true;
+
+            _albums = new PanoramaGroup("trending tracks");
+            _artists = new PanoramaGroup("trending artists");
+
+            Groups = new ObservableCollection<PanoramaGroup> { _albums, _artists };
+
+            _artists.SetSource(SampleData.Artists.Take(25));
+            _albums.SetSource(SampleData.Albums.Take(25));
+
+
+            Busy = false;
+        }
+
+        public ObservableCollection<PanoramaGroup> Groups { get; set; }
+        public bool Busy { get; set; }
+        public string Title { get; set; }
+        public int SelectedIndex { get; set; }
+        public List<Album> Albums { get; set; }
+        public List<Artist> Artists { get; set; }
+    }
+
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        [DisplayName("Genre")]
+        public int GenreId { get; set; }
+
+        [DisplayName("Artist")]
+        public int ArtistId { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        [DisplayName("Album Art URL")]
+        public string AlbumArtUrl { get; set; }
+
+        public virtual Genre Genre { get; set; }
+
+        public virtual Artist Artist { get; set; }
+    }
+
+    public partial class Genre
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public static class SampleData
+    {
+        public static List<Genre> Genres { get; set; }
+        public static List<Artist> Artists { get; set; }
+        public static List<Album> Albums { get; set; }
+        public static void Seed()
+        {
+            if (Genres != null)
+                return;
+
+            Genres = new List<Genre>
+            {
+                new Genre { Name = "Rock" },
+                new Genre { Name = "Jazz" },
+                new Genre { Name = "Metal" },
+                new Genre { Name = "Alternative" },
+                new Genre { Name = "Disco" },
+                new Genre { Name = "Blues" },
+                new Genre { Name = "Latin" },
+                new Genre { Name = "Reggae" },
+                new Genre { Name = "Pop" },
+                new Genre { Name = "Classical" }
+            };
+
+            Artists = new List<Artist>
+            {
+                new Artist { Name = "Aaron Copland & London Symphony Orchestra" },
+                new Artist { Name = "Aaron Goldberg" },
+                new Artist { Name = "AC/DC" },
+                new Artist { Name = "Accept" },
+                new Artist { Name = "Adrian Leaper & Doreen de Feis" },
+                new Artist { Name = "Aerosmith" },
+                new Artist { Name = "Aisha Duo" },
+                new Artist { Name = "Alanis Morissette" },
+                new Artist { Name = "Alberto Turco & Nova Schola Gregoriana" },
+                new Artist { Name = "Alice In Chains" },
+                new Artist { Name = "Amy Winehouse" },
+                new Artist { Name = "Anita Ward" }
+            };
+
+            Albums = new List<Album>
+            {
+                new Album {Title = "The Best Of Men At Work", Genre = Genres.Single(g => g.Name == "Rock"), Price = 8.99M, Artist = Artists.Single(a => a.Name == "Accept"), AlbumArtUrl = "/Content/Images/placeholder.gif"},
+                new Album {Title = "A Copland Celebration, Vol. I", Genre = Genres.Single(g => g.Name == "Classical"), Price = 8.99M, Artist = Artists.Single(a => a.Name == "Aaron Copland & London Symphony Orchestra"), AlbumArtUrl = "/Content/Images/placeholder.gif"},
+                new Album {Title = "Worlds", Genre = Genres.Single(g => g.Name == "Jazz"), Price = 8.99M, Artist = Artists.Single(a => a.Name == "Aaron Goldberg"), AlbumArtUrl = "/Content/Images/placeholder.gif"},
+                new Album {Title = "For Those About To Rock We Salute You", Genre = Genres.Single(g => g.Name == "Rock"), Price = 8.99M, Artist = Artists.Single(a => a.Name == "AC/DC"), AlbumArtUrl = "/Content/Images/placeholder.gif"},
+                new Album {Title = "Let There Be Rock", Genre = Genres.Single(g => g.Name == "Rock"), Price = 8.99M, Artist = Artists.Single(a => a.Name == "AC/DC"), AlbumArtUrl = "/Content/Images/placeholder.gif"},
+                new Album {Title = "Balls to the Wall", Genre = Genres.Single(g => g.Name == "Rock"), Price = 8.99M, Artist = Artists.Single(a => a.Name == "Accept"), AlbumArtUrl = "/Content/Images/placeholder.gif"},
+                new Album {Title = "Restless and Wild", Genre = Genres.Single(g => g.Name == "Rock"), Price = 8.99M, Artist = Artists.Single(a => a.Name == "Accept"), AlbumArtUrl = "/Content/Images/placeholder.gif"},
+            };
+        }
+
     }
 }
